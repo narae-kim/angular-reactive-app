4dd1388f8b3fcf57f4960ad31bceb837d3e00960
Narae Kim <naraekimw@gmail.com>
2023-11-20 06:53:19 +0000

    Learning template-driven forms

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'reactive-forms-app'`, () => {
+  it(`should have as title 'routing-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('reactive-forms-app');
+    expect(app.title).toEqual('routing-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('reactive-forms-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('routing-app app is running!');
   });
 });
